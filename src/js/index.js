19d4a740d57749e63b2d47451f8c4addd56e1ddb
allEyezOnCode <alexandredewilde2001@gmail.com>
2021-01-15 22:56:19 +0100

    add images and clean

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 var encrypt = new JSEncrypt();
 function encryptMessage() {
     encrypt.setPublicKey(document.getElementById('rsa-pub-key').value);
-    document.getElementById("encrypted-text").value = encrypt.encrypt(document.getElementById('text').value);
+    document.getElementById("encrypted-text").value = encrypt.encrypt(document.getElementById('unencrypted-text').value);
 }
 
 function decryptMessage() {
     encrypt.setPrivateKey(document.getElementById('rsa-private-key').value);
-    document.getElementById("text").value = encrypt.decrypt(document.getElementById('encrypted-text').value); 
+    document.getElementById("unencrypted-text").value = encrypt.decrypt(document.getElementById('encrypted-text').value); 
 }
 
 document.getElementById('button-encrypt').addEventListener('click', encryptMessage);
